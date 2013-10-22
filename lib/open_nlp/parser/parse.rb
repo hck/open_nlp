3a63bf5462ce223411c0536d721a14245861fa76
hck <alexsvirin@gmail.com>
2013-10-22 23:59:47 +0300

    Minor refactor in Parser::Parse#tree_bank_string

--- lib/open_nlp/parser/parse.rb
+++ lib/open_nlp/parser/parse.rb
@@ -13,27 +13,20 @@ module OpenNlp
     end
 
     def tree_bank_string
-      span = j_instance.getSpan
-      text = j_instance.getText
-      type = j_instance.getType
-      start = span.getStart
+      span, text, type, res = j_instance.getSpan, j_instance.getText, j_instance.getType, ''
+      start                 = span.getStart
 
-      res = ''
+      res << "(#{type} " if type != Java::opennlp.tools.parser.AbstractBottomUpParser::TOK_NODE
 
-      res << "(#{type} " unless type == Java::opennlp.tools.parser.AbstractBottomUpParser::TOK_NODE
-
-      j_instance.getChildren.each do |c|
-        s = c.span
-        res << text[start..s.getStart-1] if start < s.getStart
-
-        subtree = self.class.new(c).tree_bank_string
-        res << subtree if subtree
-        start = s.getEnd
+      j_instance.getChildren.each do |child|
+        child_span = child.span
+        res << text[start..child_span.getStart-1] if start < child_span.getStart
+        res << self.class.new(child).tree_bank_string
+        start = child_span.getEnd
       end
 
       res << text[start..span.getEnd-1] if start < span.getEnd
-
-      res << ")" unless type == Java::opennlp.tools.parser.AbstractBottomUpParser::TOK_NODE
+      res << ")" if type != Java::opennlp.tools.parser.AbstractBottomUpParser::TOK_NODE
 
       res
     end
@@ -41,13 +34,11 @@ module OpenNlp
     def code_tree
       kids = j_instance.getChildren
 
-      kids.inject([]) do |acc,kid|
-        data = {:type => kid.getType, :parent_type => self.j_instance.getType, :token => kid.toString}
+      kids.each_with_object([]) do |kid, acc|
+        data    = { :type => kid.getType, :parent_type => self.j_instance.getType, :token => kid.toString }
         subtree = self.class.new(kid).code_tree
         data[:children] = subtree unless subtree.empty?
         acc << data
-
-        acc
       end
     end
   end
