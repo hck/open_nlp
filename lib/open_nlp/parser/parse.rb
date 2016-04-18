1e78619a783734ae32807f06afbb4089a2218aeb
hck <alexsvirin@gmail.com>
2016-04-18 10:49:39 +0200

    Add comments for Parser::Parse class, minor style fixes.

--- lib/open_nlp/parser/parse.rb
+++ lib/open_nlp/parser/parse.rb
@@ -6,15 +6,25 @@ module OpenNlp
 
     self.java_class = Java::opennlp.tools.parser.Parse
 
+    # Initializes instance of Parser::Parse
+    #
+    # @param [Java::opennlp.tools.parser.Parse] java_instance
     def initialize(java_instance)
-      raise ArgumentError, "java_instance must be an instance of #{self.class.java_class.name}" unless java_instance.is_a?(self.class.java_class)
+      unless java_instance.is_a?(self.class.java_class)
+        fail ArgumentError, "java_instance must be an instance of #{self.class.java_class.name}"
+      end
 
       @j_instance = java_instance
     end
 
+    # Composes tree bank string, nested string representation of sentence parts, parts-of-speech and words,
+    # for example:
+    #   '(TOP (S (NP (DT The) (JJ red) (NN fox)) (VP (VBZ sleeps) (ADVP (RB soundly))) (. .)))'
+    #
+    # @return [String]
     def tree_bank_string
       span, text, type, res = j_instance.getSpan, j_instance.getText, j_instance.getType, ''
-      start                 = span.getStart
+      start = span.getStart
 
       res << "(#{type} " if type != Java::opennlp.tools.parser.AbstractBottomUpParser::TOK_NODE
 
@@ -31,11 +41,20 @@ module OpenNlp
       res
     end
 
+    # Composes array representation of sentence tree where
+    # each hash has following fields:
+    #
+    #  :type => <[String] node type>,
+    #  :parent_type => <[String] type of parent node>,
+    #  :token => <[String] current token>,
+    #  :children => <Array[Hash] array of child nodes hashes>
+    #
+    # @return [Array<Hash>]
     def code_tree
       kids = j_instance.getChildren
 
       kids.each_with_object([]) do |kid, acc|
-        data    = { :type => kid.getType, :parent_type => self.j_instance.getType, :token => kid.toString }
+        data = { :type => kid.getType, :parent_type => self.j_instance.getType, :token => kid.toString }
         subtree = self.class.new(kid).code_tree
         data[:children] = subtree unless subtree.empty?
         acc << data
