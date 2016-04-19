e76099820551568ab494c059fdb2b91c5368c2c9
hck <alexsvirin@gmail.com>
2016-04-19 22:00:45 +0200

    RSpec 3 spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,28 @@ require 'java'
 require 'open_nlp'
 
 FIXTURES_DIR = File.join(File.dirname(__FILE__), 'fixtures')
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+
+  config.disable_monkey_patching!
+
+  config.warnings = true
+
+  config.profile_examples = 10
+
+  config.order = :random
+
+  Kernel.srand config.seed
+end
